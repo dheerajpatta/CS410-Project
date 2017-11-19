e1d1bb4514bad30e7afb6aeedf47f36345665cd4
Ilean Monterrubio Jr <imonterrubio@uh.edu>
2017-11-19 15:58:35 -0600

    reviewReducer reduces the file to 100 comments
    Next step is to do some analysis on the comment to make sure is in English.

--- reviewReducer.py
+++ reviewReducer.py
@@ -3,3 +3,11 @@ CS410 - Project - Yelp Sentimate Analysis
 This file will take the review.json file and reduce it to a managable size of 100 for testing.
 """
 import json
+import metapy
+from itertools import islice
+
+a = open('new-review.json', 'w+')
+f = open('review.json')
+for line in islice(f, 100):
+    a.write(line)
+a.close()
